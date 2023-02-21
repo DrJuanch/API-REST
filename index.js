3de0282ea47f8b7f6abaa5bac9bfb674a07b5662
DrJuanch <juanjo88023@gmail.com>
2023-02-20 23:11:56 -0500

    My server with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello APIS")
+const express = require('express');
+
+const app = express();
+const port = 3030;
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is my server in express');
+});
+
+app.listen(port, () =>{
+  console.log(`My port is ${port}`);
+});
