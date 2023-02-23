1ba549b409169cd6a6e13e4c879a60ad5bbba967
DrJuanch <juanjo88023@gmail.com>
2023-02-23 10:45:18 -0500

    Variables de entorno

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ const db = require('mongoose');
 db.Promise = global.Promise;
 db.set('strictQuery', true)
 
-async function connect (URL) {
-  await db.connect(URL,{useNewUrlParser: true,useUnifiedTopology: true})
+async function connect () {
+  const MONGO_URI= process.env.MONGO_URI;
+  await db.connect(MONGO_URI,{useNewUrlParser: true,useUnifiedTopology: true})
     .then(() => console.log('[db] connected successfuly'))
     .catch(err => console.error('[db]', err));
 };
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { urlencoded } = require('body-parser');
-// const response = require('./route/routes');
+const PORT = process.env.PORT || 3030;
 const db = require('./db');
 const router = require('./network/routes');
 
 // This is my server I created with express
-db('mongodb+srv://administrador:administrador1342**@messages.7rqujqp.mongodb.net/?retryWrites=true&w=majority');
 const app = express();
-const port = 3030;
-
 // We use it to parser the response's body in different types of information (JSON, urlencoded...ETC)
 app.use(bodyParser.json());
 app.use(bodyParser.json(urlencoded({extended: false})));
-// app.use(router);
 
-//
 router(app);
 
-
-app.listen(port, () =>{
-  console.log(`My port is ${port}`);
-});
+db()
+app.listen(PORT, () => {
+    console.log('API lista por el puerto ', PORT)
+})
